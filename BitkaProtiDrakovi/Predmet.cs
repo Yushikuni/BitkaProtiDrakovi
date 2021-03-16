8615fe3de53aae1bd3a5198cecdcb6090915d7ee
Kvetuse "Pein" Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-03-16 14:18:24 +0100

    Update Predmet.cs

--- BitkaProtiDrakovi/Predmet.cs
+++ BitkaProtiDrakovi/Predmet.cs
@@ -2,7 +2,7 @@
 {
     class Predmet
     {
-        // Vlastnosti predmetu
+        // Vlastnosti predmětů
         public string Nazev { get; set; }
         public int Sila { get; set; }
         public int Utocnost { get; set; }
@@ -11,7 +11,7 @@
         public bool JeZbran { get; set; }       // true = je zbraň, false = není zbraň
         public bool JeObourucni { get; set; }   // true = je obouruční zbraň, false = není obouruční zbraň
 
-        // Konstruktor noveho predmetu
+        // Konstruktor nového předmětu
         public Predmet(string nazev, int sila, int utocnost, int obrana, int vaha, bool jeZbran, bool jeObourucni)
         {
             Nazev = nazev;
@@ -23,7 +23,7 @@
             JeObourucni = jeObourucni;
         }
 
-        // Metoda pro vypis predmetu
+        // Metoda pro výpis předmětů
         public override string ToString()
         {
             string ret = "";
